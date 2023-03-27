209c226b5abf58718df97ba593a5f8f6c4e6c54e
anhyeongkwan <bombom841@naver.com>
2023-03-27 23:37:10 +0900

    - 프로젝트 생성

---
+++ src/main/java/com/hyeongkwan/springbootboard/controller/controller.java
@@ -0,0 +1,13 @@
+package com.hyeongkwan.springbootboard.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Log4j2
+@RequestMapping("/hello")
+@RequiredArgsConstructor
+public class controller {
+}
